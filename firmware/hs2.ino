54d7293d9e6efb52700ae591cf6a14ce1141754f
Aaron Bieber <aaron@bolddaemon.com>
2020-12-27 14:55:47 -0700

    prefix everything with wstation_

--- firmware/hs2.ino
+++ firmware/hs2.ino
@@ -13,43 +13,43 @@ void getData(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 	float hpascals = sensor.readPressure() / 100;
 	server.httpSuccess();
 	if (type == WebServer::GET) {
-		P(humidity) =	"# HELP humidity Last sampled humidity of weather shield\r\n"
-				"# TYPE humidity gauge\r\n";
+		P(humidity) =	"# HELP wstation_humidity Last sampled humidity of weather shield\r\n"
+				"# TYPE wstation_humidity gauge\r\n";
 
-		P(baroTemp) =	"# HELP barometer_temperature_f Last sampled barometer temperature of weather shield\r\n"
-				"# TYPE barometer_temperature_f gauge\r\n";
+		P(baroTemp) =	"# HELP wstation_barometer_temperature_f Last sampled barometer temperature of weather shield\r\n"
+				"# TYPE wstation_barometer_temperature_f gauge\r\n";
 
-		P(temp) =	"# HELP temp_f Last sampled temperature in F of weather shield\r\n"
-				"# TYPE temp_f gauge\r\n";
+		P(temp) =	"# HELP wstation_temp_f Last sampled temperature in F of weather shield\r\n"
+				"# TYPE wstation_temp_f gauge\r\n";
 
-		P(pressHPA) =	"# HELP pressure_hpa Last sampled pressure in hectopascals from weather shield\r\n"
-				"# TYPE pressure_hpa gauge\r\n";
+		P(pressHPA) =	"# HELP wstation_pressure_hpa Last sampled pressure in hectopascals from weather shield\r\n"
+				"# TYPE wstation_pressure_hpa gauge\r\n";
 
-		P(pressHG) =	"# HELP pressure_hg Last sampled pressure in inches if mercury from weather shield\r\n"
-				"# TYPE pressure_hg gauge\r\n";
+		P(pressHG) =	"# HELP wstation_pressure_hg Last sampled pressure in inches if mercury from weather shield\r\n"
+				"# TYPE wstation_pressure_hg gauge\r\n";
 
 		server.printP(humidity);
-		server.printf("humidity{name=\"%s\"} ", stationName);
+		server.printf("wstation_humidity{name=\"%s\"} ", stationName);
 		server.print(sensor.getRH());
 		server.printCRLF();
 
 		server.printP(baroTemp);
-		server.printf("barometer_temperature_f{name=\"%s\"} ", stationName);
+		server.printf("wstation_barometer_temperature_f{name=\"%s\"} ", stationName);
 		server.print(sensor.readBaroTempF());
 		server.printCRLF();
 
 		server.printP(temp);
-		server.printf("temp_f{name=\"%s\"} ", stationName);
+		server.printf("wstation_temp_f{name=\"%s\"} ", stationName);
 		server.print(sensor.getTempF());
 		server.printCRLF();
 
 		server.printP(pressHPA);
-		server.printf("pressure_hpa{name=\"%s\"} ", stationName);
+		server.printf("wstation_pressure_hpa{name=\"%s\"} ", stationName);
 		server.print(hpascals);
 		server.printCRLF();
 
 		server.printP(pressHG);
-		server.printf("pressure_hg{name=\"%s\"} ", stationName);
+		server.printf("wstation_pressure_hg{name=\"%s\"} ", stationName);
 		server.print(hpascals * 0.0295300);
 		server.printCRLF();
 	}
