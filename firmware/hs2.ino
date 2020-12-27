46d9a514f2ee371a11d84df18ce03827d277a751
Aaron Bieber <aaron@bolddaemon.com>
2020-12-27 14:09:27 -0700

    promersize

--- firmware/hs2.ino
+++ firmware/hs2.ino
@@ -1,11 +1,12 @@
-#define RGB_NOTIFICATIONS_CONNECTING_ONLY
+#define RGB_NOTIFICATIONS_CONNECTING_ONLY 1
+#define NAME GreenHouse
 
 #include <WebServer.h>
 #include <SparkFun_Photon_Weather_Shield_Library.h>
 
 Weather sensor;
 
-WebServer webserver("/", 80);
+WebServer webserver("/metrics", 80);
 
 void getData(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 	float hpascals = sensor.readPressure() / 100;
@@ -13,23 +14,23 @@ void getData(WebServer &server, WebServer::ConnectionType type, char *, bool) {
 	if (type == WebServer::GET) {
 		P(humidity) =	"# HELP humidity Last sampled humidity of weather shield\r\n"
 				"# TYPE humidity gauge\r\n"
-				"humidity ";
+				sprintf("humidity{name=\"%s\"}", NAME);
 
 		P(baroTemp) =	"# HELP barometer_temperature_f Last sampled barometer temperature of weather shield\r\n"
 				"# TYPE barometer_temperature_f gauge\r\n"
-				"barometer_temperature_f ";
+				sprintf("barometer_temperature_f{name=\"%s\"} ", NAME};
 
 		P(temp) =	"# HELP temp_f Last sampled temperature in F of weather shield\r\n"
 				"# TYPE temp_f gauge\r\n"
-				"temp_f ";
+				sprintf("temp_f{name=\"%s\"} ", NAME);
 
 		P(pressHPA) =	"# HELP pressure_hpa Last sampled pressure in hectopascals from weather shield\r\n"
 				"# TYPE pressure_hpa gauge\r\n"
-				"pressure_hpa ";
+				sprintf("pressure_hpa{name=\"%s\"} ", NAME);
 
 		P(pressHG) =	"# HELP pressure_hg Last sampled pressure in inches if mercury from weather shield\r\n"
 				"# TYPE pressure_hg gauge\r\n"
-				"pressure_hg ";
+				sprintf("pressure_hg{name=\"%s\"} ", NAME);
 
 		server.printP(humidity);
 		server.print(sensor.getRH());
